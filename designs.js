ce84c2a22343daa03c9de3602656467c85e740dd
Diana Sousa <dianac182@gmail.com>
2018-01-04 14:05:47 +0100

    feat: select color input

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
-// Select size input
+$(function(){
 
-// When size is submitted by the user, call makeGrid()
+    // Select color input
 
-function makeGrid() {
+    $("#colorPicker").change(function() {
+        let colorPicked;
+        colorPicked= $("#colorPicker").val();
+        $("#colorPicker").attr("value",colorPicked);
+    });
 
-// Your code goes here!
+    // Select size input
 
-}
+
+    /*$("#submit").submit(function() {
+        let N = $("#input_height").val();
+        return N;
+        console.log(N);
+        let M = $("#input_width").val();
+        return M;
+        console.log(M);
+        makeGrid(N,M);
+    });*/
+    /*console.log(N);
+    console.log(M);*/
+
+
+    // When size is submitted by the user, call makeGrid()
+
+   /* function makeGrid(N,M) {
+        for(let i=1; i<=N; i=i+1) {
+            $("#pixel_canvas").append("tr");
+            for(let j=1; j<=M; j=j+1) {
+                $("table tr:last-child").append("td");
+            };
+        };
+    };*/
+
+});
+
+
