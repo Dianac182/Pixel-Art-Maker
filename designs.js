21203f79e845d29198805386c3ad38ab2d814f1e
Diana Sousa <dianac182@gmail.com>
2018-01-05 18:56:33 +0100

    feat: create grid after size is submitted

--- designs.js
+++ designs.js
@@ -1,40 +1,44 @@
-$(function(){
-
+$(function () {
     // Select color input
 
     $("#colorPicker").change(function() {
         let colorPicked;
-        colorPicked= $("#colorPicker").val();
+        colorPicked = $("#colorPicker").val();
         $("#colorPicker").attr("value",colorPicked);
     });
 
     // Select size input
 
+    /*TODO: try another solution rather than removing and adding the button*/
 
-    /*$("#submit").submit(function() {
-        let N = $("#input_height").val();
-        return N;
-        console.log(N);
-        let M = $("#input_width").val();
-        return M;
-        console.log(M);
+    $('form input:last-child').click(function() {
+        let N;let M;
+        N = $('#input_height').val();
+        $('#input_height').attr('value',N);
+        M = $('#input_width').val();
+        $('#input_width').attr('value',M);
+        console.log('You clicked!');
+        $(this).remove();
+        $('form').append('<input type="submit" value="Submit">')
         makeGrid(N,M);
-    });*/
-    /*console.log(N);
-    console.log(M);*/
-
+    });
 
     // When size is submitted by the user, call makeGrid()
 
-   /* function makeGrid(N,M) {
-        for(let i=1; i<=N; i=i+1) {
-            $("#pixel_canvas").append("tr");
-            for(let j=1; j<=M; j=j+1) {
-                $("table tr:last-child").append("td");
-            };
-        };
-    };*/
+     function makeGrid(N,M) {
+         for(let i=1; i<=N; i=i+1) {
+             $('#pixel_canvas').append('<tr></tr>');
+             for(let j=1; j<=M; j=j+1) {
+                 $('table tr:last-child').append('<td></td>');
+             };
+         };
+     };
+
+     // Draw the pixel art, by clicking on the desire square.
+
+
+
+
+
 
 });
-
-
