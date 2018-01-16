229d70238f1bdc0cef457750e976a8441edcb7cd
Diana Sousa <dianac182@gmail.com>
2018-01-16 20:10:33 +0100

    feat: change css style and add some improvements
    Css changes: styling of background, buttons, color picker and grid.
    Improvement of event listener for the createButton.
    Creation of clearButton for clearing the color of the grid.

--- designs.js
+++ designs.js
@@ -1,45 +1,69 @@
 $(function () {
+
+    //Define variables
+
+    const colorPicker = $('#colorPicker');
+    const tableCanvas = $('#pixel_canvas');
+    const createButton = $('#create_grid');
+    const clearButton = $('#clear_grid');
+    const gridHeight = $('#input_height');
+    const gridWidth = $('#input_width');
+
+    let color = "#000";
+
+
     // Select color input
 
-    $("#colorPicker").change(function() {
-        let colorPicked;
-        colorPicked = $("#colorPicker").val();
-        $("#colorPicker").attr("value",colorPicked);
+    colorPicker.change(function() {
+        color = colorPicker.val();
+        colorPicker.attr("value",color);
     });
 
     // Select size input
 
-    /*TODO: try another solution rather than removing and adding the button*/
-
-    $('form input:last-child').click(function() {
-        let N;let M;
-        N = $('#input_height').val();
-        $('#input_height').attr('value',N);
-        M = $('#input_width').val();
-        $('#input_width').attr('value',M);
-        $(this).remove();
-        $('form').append('<input type="submit" value="Submit">');
+    createButton.click(function(evt) {
+        let N = gridHeight.val();
+        gridHeight.attr('value',N);
+        let M = gridWidth.val();
+        gridWidth.attr('value',M);
+        for(let i=1; i<=N; i++) {
+            $('#pixel_canvas tr').remove();
+        }
         makeGrid(N,M);
+        evt.preventDefault();
     });
 
     // When size is submitted by the user, call makeGrid()
 
-     function makeGrid(N,M) {
-         for(let i=1; i<=N; i=i+1) {
-             $('#pixel_canvas').append('<tr></tr>');
-             for(let j=1; j<=M; j=j+1) {
-                 $('table tr:last-child').append('<td></td>');
-             };
-         };
-     };
+    function makeGrid(N,M) {
+        for(let i=1; i<=N; i=i+1) {
+            tableCanvas.append('<tr></tr>');
+            for(let j=1; j<=M; j=j+1) {
+                $('table tr:last-child').append('<td></td>');
+            }
+        }
+    }
 
-     // Draw the pixel art, by clicking on the desire square.
+    // Draw the pixel art, by clicking on the desire square.
 
-    $('table').click(function(evt) {
+    tableCanvas.click(function(evt) {
         $(evt.target).toggleClass('color_changed');
-        let colorPicked = $(evt.target).hasClass('color_changed') ? $("#colorPicker").val() : 'white';
-        $(evt.target).css('background-color',colorPicked);
+        let colorBg = $(evt.target).hasClass('color_changed') ? color : 'white';
+        $(evt.target).css('background-color',colorBg);
     });
 
+    /*function fillin2(evt) {
+        let color = $('#colorPicker').val();
+        $(evt.target).attr('style', 'background-color: color');
+    }
+    let pixel = $('#pixel_canvas').find('td');
+    pixel.on('click',fillin2);*/
+
+    // Clear grid pressing button
+
+    clearButton.click(function(evt) {
+        evt.preventDefault();
+        $('td').css('background-color','white');
+    });
 
 });
