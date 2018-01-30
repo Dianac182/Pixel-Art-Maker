f014d297c2c50e66d53f1e8e327aca6f7bb1a482
Diana Sousa <dianac182@gmail.com>
2018-01-30 16:34:48 +0100

    docs: follow style guide

--- designs.js
+++ designs.js
@@ -2,23 +2,24 @@ $(function () {
 
     //Define variables
 
-    const colorPicker = $('#colorPicker');
-    const tableCanvas = $('#pixel_canvas');
-    const createButton = $('#create_grid');
-    const clearButton = $('#clear_grid');
-    const gridHeight = $('#input_height');
-    const gridWidth = $('#input_width');
+    const colorPicker = $('#color-picker');
+    let tableCanvas = $('#pixel-canvas');
+    const createButton = $('#create-grid');
+    const clearButton = $('#clear-grid');
+    let gridHeight = $('#input-height');
+    let gridWidth = $('#input-width');
 
-    let color = "#000";
 
-    // Select color input
+    let color = '#000';            // Default color of the grid.
+
+    // Select color input and save the color picked
 
     colorPicker.change(function() {
         color = colorPicker.val();
-        colorPicker.attr("value",color);
+        colorPicker.attr('value', color);
     });
 
-    // Select size input
+    // Select size input after pressing createButton
 
     createButton.click(function(evt) {
         let N = gridHeight.val();
@@ -26,26 +27,25 @@ $(function () {
         let M = gridWidth.val();
         gridWidth.attr('value',M);
         for(let i=1; i<=N; i++) {
-            $('#pixel_canvas tr').remove();
+            $('#pixel-canvas tr').remove();
         }
 
+        // Depending on the inputs, show a message alert when these are not acceptable
 
-        if((parseInt(N) < 1) || (parseInt(M) < 1)) {
-            alert("You entered a value less than 1! Please try a new one.");
-        } else if((parseInt(N) > 100) || (parseInt(M) > 100)) {
-            alert("You entered a value greater than 100! Please try a new one.");
-        } else if((N != parseInt(N, 10)) || (M != parseInt(M, 10))) {
-            alert("Your input is not a integer! Please try again.");
+        if((N != parseInt(N, 10)) || (M != parseInt(M, 10))) {
+            alert('Some of your inputs are not integers! Please try again.');
+        } else if((parseInt(N) <= 1) || (parseInt(M) <= 1) || (parseInt(N) > 100) || (parseInt(M) > 100)) {
+            alert('Some of your inputs are incorrect! Please try again.');
         } else {
             makeGrid(N,M);
         }
+
         evt.preventDefault();
     });
 
-    // When size is submitted by the user, call makeGrid()
+    //Declaration of makeGrid() function
 
-    function makeGrid(N,M) {
-        for(let i=1; i<=N; i++) {
+    function makeGrid(N,M) {for(let i=1; i<=N; i++) {
             tableCanvas.append('<tr></tr>');
             for(let j=1; j<=M; j++) {
                 $('table tr:last-child').append('<td></td>');
@@ -56,9 +56,9 @@ $(function () {
     // Draw the pixel art, by clicking on the desire square.
 
     tableCanvas.click(function(evt) {
-        $(evt.target).toggleClass('color_changed');
-        let colorBg = $(evt.target).hasClass('color_changed') ? color : 'white';
-        $(evt.target).css('background-color',colorBg);
+        $(evt.target).toggleClass('color-changed');       // When a cell is clicked, the "color-changed" class, associated to the color picked, is added.
+        let colorBg = $(evt.target).hasClass('color-changed') ? color : 'white';
+        $(evt.target).css('background-color', colorBg);
     });
 
     // Clear grid pressing button
